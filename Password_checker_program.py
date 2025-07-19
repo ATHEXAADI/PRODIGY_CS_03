4cf7b2ef09ab36afa40024d94d7c0e1752778c64
Aditya Kumar Singh <kumarsinghaditya471@gmail.com>
2025-07-19 15:57:48 +0530

    Update Password_checker_program.py

--- Password_checker_program.py
+++ Password_checker_program.py
@@ -1,3 +1,5 @@
+## Build a tool that assesses the strength of a password based on criteria such as length, presence of uppercase and lowercase letters, numbers, and special characters. and also Provide feedback to user on the password's strength.
+
 import tkinter as tk
 import re
 
